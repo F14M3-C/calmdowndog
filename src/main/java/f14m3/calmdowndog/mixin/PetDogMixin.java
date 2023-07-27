f850ca3e9f0f42568f1d5a0596f3d5fe289c1fd6
F14M3-C <augustinas142@gmail.com>
2023-07-27 08:26:06 +0300

    Add the actual mod source

---
+++ src/main/java/f14m3/calmdowndog/mixin/PetDogMixin.java
@@ -0,0 +1,29 @@
+package f14m3.calmdowndog.mixin;
+
+import net.minecraft.entity.EntityStatuses;
+import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WolfEntity.class)
+public class PetDogMixin {
+	@Inject(method="interactMob(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+	public void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cil) {
+		World world = player.getWorld();
+		WolfEntity wolf = ((WolfEntity)(Object)this);
+		if (player.isSneaking() && wolf.isTamed()) {
+			world.sendEntityStatus(wolf, EntityStatuses.ADD_POSITIVE_PLAYER_REACTION_PARTICLES);
+			if (wolf.isOwner(player)) {
+				wolf.stopAnger();
+			}
+			cil.setReturnValue(ActionResult.SUCCESS);
+		}
+	}
+}
